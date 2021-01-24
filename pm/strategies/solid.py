edee965d5dec633a6d559c4a09a698d956a38cfc
Claire Vernade <claire.vernade@gmail.com>
2021-01-24 13:05:01 +0100

    solid corrected

--- pm/strategies/solid.py
+++ pm/strategies/solid.py
@@ -1,14 +1,13 @@
+from pm.strategy import Strategy
 import numpy as np
-import logging
-import cvxpy as cp
-
 from scipy.linalg import cho_solve, cho_factor
 
-from pm.strategy import Strategy
 from pm.utils import difference_matrix, psd_norm_squared
+import cvxpy as cp
+
 
 class Solid(Strategy):
-    def __init__(self, game, estimator, lambda_1=0., z_0=100, alpha_l=0.1, alpha_w = 0.5, lambda_max=10):
+    def __init__(self, game, estimator, lambda_1=0., z_0=100, alpha_l=0.1, alpha_w = 0.5, lambda_max=10, reset=True):
         #check default values of parameters
         super().__init__(game, estimator)
         self.lambda_t = lambda_1
@@ -29,10 +28,12 @@ class Solid(Strategy):
         self.w_t = np.ones(self.K) / self.K #uniform over actions
         #for multi context bandit we need a matrix (nb_context x nb_actions)
         self._t = 1
+        self.reset = reset
 
     def compute_nu(self, indices):
         """
         Compute the alternative parameters and Vnorm for each cell
+        Never called, just for debugging
         """
         d = self._game.d
         X = self._game.get_actions(indices)
@@ -71,7 +72,7 @@ class Solid(Strategy):
         #Warning : may be numerically unstable
         Vw = self.get_Vw(indices)
         VW_eps = self.get_Vw(indices, eps=0.01)
-        diff_eps = np.min(self.get_info_ratios(VW_eps)) - np.min(self.get_info_ratios(Vw))
+        diff_eps =  self.get_info_ratios(VW_eps) - self.get_info_ratios(Vw)
         diff_eps /= 0.01
 
 
@@ -100,6 +101,7 @@ class Solid(Strategy):
 
         # update w_t
 
+
         #compute q_t(x,a)
         q_t= self.compute_q()
 
@@ -110,42 +112,46 @@ class Solid(Strategy):
 
         #update lambda_t :
         Vw = self.get_Vw(indices)
-        min_Vw_norm = np.min(self.get_info_ratios(Vw))
+        min_Vw_norm = self.get_info_ratios(Vw)
 
         g_t = min_Vw_norm + np.dot(self.w_t, np.sqrt(self._estimator.lls.beta(1/self.explo_rounds) * self._estimator.var(indices))) - 1/self.z_k
         # print(g_t)
         self.lambda_t = np.max([0,np.min([self.lambda_t - self.alpha_l*g_t, self.lambda_max])])
 
 
+
+
     def get_info_ratios(self,  V_matrix):
         # Warning, also updates the winner and means
+        #
 
-        indices = self._game.get_indices()
-        X = self._game.get_actions(indices)
-        self._means = self._estimator.lls.mean(self._game.get_actions(indices))
-        self._winner = np.argmax(self._means)
-        X_win = X[self._winner,:]
+        ## Johannes's implementation
+        # I don't understand why the second best gap is also the second best of the ratio
+        gaps = self._means[self._winner] - self._means
+        gaps[self._winner] = np.inf
+        second_best = np.argmin(gaps)
+        x_best = self._game.get_actions(self._winner)
+        x_2best = self._game.get_actions(second_best)
+        # alt_ms = gaps[second_best]**2/self._estimator.lls.var(x_best - x_2best)
 
-        sq_norms = psd_norm_squared(X - X_win, V_matrix)
-        sq_norms[self._winner] = 1 ## to avoid numerical problems, returns a null ratio
-        sq_gaps = (self._means - self._means[self._winner])**2
+        sq_gap = gaps[second_best]**2
+        # same as in estimator.lls.var(x) but with the general V_matrix
+        cholesky = cho_factor(V_matrix)
+        sol_x = cho_solve(cholesky, (x_best-x_2best).T).T
+        sq_norm =  np.sum((x_best-x_2best)*sol_x, axis=-1)
 
-<<<<<<< HEAD
-        print('sq_norms'+str(sq_norms))
-        print('sq_gaps'+str(sq_gaps))
+        return  sq_gap / sq_norm
 
-        ratios = sq_gaps / sq_norms
 
-        print(ratios)
-
-        return  ratios
-=======
-        return  np.divide(sq_gaps,sq_norms)
->>>>>>> bd18fbe0652a64c5c6538fff09f201df71f3eb17
 
 
     def get_next_action(self):
         indices = self._game.get_indices()
+
+
+        self._means = self._estimator.lls.mean(self._game.get_actions(indices))
+        self._winner = np.argmax(self._means)
+
         #same as in ids, but article takes delta=1/n and uses n instead of logdet(V_t)
         _t = max(2, self._t)
         beta_t = self._estimator.lls.beta(1/(_t * np.log(_t)))
@@ -155,29 +161,25 @@ class Solid(Strategy):
         # self._min_V_norm = min_V_norm
 
         # compute the minimum "information ratio" as in Eq 80 of Ap K
-        inf_ratios = self.get_info_ratios(self._estimator.lls.V)
+        self._min_ratio = self.get_info_ratios(self._estimator.lls.V)
         # print(inf_ratios)
 
         #temporary:
-        nu, V_norm = self.compute_nu(indices)
+        # nu, V_norm = self.compute_nu(indices)
 
-        # compute minimum V-norm without winner
-        V_norm[self._winner] = np.Infinity
-        ms = np.min(V_norm)
-        print('ms:'+str(ms))
+        # # compute minimum V-norm without winner
+        # V_norm[self._winner] = np.Infinity
+        # ms = np.min(V_norm)
+        # print('ms:'+str(ms))
 
 
-
-        self._min_ratio = np.min(inf_ratios)
-
-        print('min_ratio'+str(self._min_ratio)) # should be equal to self.ms in asymp-ids
+        # print('min_ratio'+str(self._min_ratio)) # should be equal to self.ms in asymp-ids
         # print(beta_t)
 
         if self._min_ratio > beta_t:
             #exploitation step
             #recompute at every round because estimator changes
-            logging.debug(f"Exploitation round: {self._t}")
-
+            # print('exploitation !')
 
             return self._winner
 
@@ -190,23 +192,29 @@ class Solid(Strategy):
 
             if self.phase_length == self.p_k:
                 #update phase counters
-                logging.info('increasing phase: '+str(self.phase_length))
+                print('increasing phase: '+str(self.phase_length))
                 self.phase += 1
                 self.phase_length = 0
                 self.z_k *= np.exp(self.phase / (self.phase -1 ))
                 self.z_k = np.floor(self.z_k)
                 self.p_k = np.int(self.z_k * np.exp(self.phase)) #removed the 2* in exp
-                logging.info('next phase is ' + str(self.p_k))
+                print('next phase is ' + str(self.p_k))
                 # updates alphas
                 self.alpha_w , self.alpha_l = self.update_alphas()
                 #reset
-                self.w_t =  np.ones(self.K) / self.K #uniform over actions #uniform over actions
-                self.lambda_t = self.lambda_1
+                if self.reset:
+                    self.w_t =  np.ones(self.K) / self.K #uniform over actions #uniform over actions
+                    self.lambda_t = self.lambda_1
             return chosen_action
 
     def add_observations(self, indices, y):
+
         super().add_observations(indices, y)
+
         self._t += 1  # increase step counter each time we get the data
 
     def id(self):
-        return "Solid"
+        if self.reset:
+            return "Solid"
+        else:
+            return "Solid++"
